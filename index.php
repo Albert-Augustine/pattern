b77f4206cdfdd96566c6a14649e1f886e6efe8fb
OEM Configuration (temporary user) <albertaugustine1477gmail.com>
2021-03-26 10:23:13 +0530

    Makking pattern

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+for ($x = 0; $x <= 10; $x++) {
+	for ($y = 0; $y <= $x; $y++) {
+  		echo "*\n\n";
+	}
+    echo"<br>";
+ }
+ for ($x = 10; $x >= 0; $x--) {
+	for ($y = 0; $y <= $x; $y++) {
+  		echo "*\n\n";
+	}
+    echo"<br>";
+ }
+ 
+?>  
+
+</body>
+</html>
